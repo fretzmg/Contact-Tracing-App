c01d784101937218b9b20992ad9d86b65de3ffec
John Fretz Magabilin <magabilinfrets@gmail.com>
2021-05-29 14:34:35 +0800

    Add project files.

---
+++ Contact Tracing App/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace Contact_Tracing_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstNametxtbx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LastNametxtbx = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Agetxtbx = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Contacttxtbx = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Emailtxtbx = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Streettxtbx = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Citytxtbx = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Provincetxtbx = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.Countrytxtbx = new System.Windows.Forms.TextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FirstNametxtbx
+            // 
+            this.FirstNametxtbx.Location = new System.Drawing.Point(98, 47);
+            this.FirstNametxtbx.Name = "FirstNametxtbx";
+            this.FirstNametxtbx.Size = new System.Drawing.Size(176, 27);
+            this.FirstNametxtbx.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Last Name: ";
+            // 
+            // LastNametxtbx
+            // 
+            this.LastNametxtbx.Location = new System.Drawing.Point(98, 80);
+            this.LastNametxtbx.Name = "LastNametxtbx";
+            this.LastNametxtbx.Size = new System.Drawing.Size(176, 27);
+            this.LastNametxtbx.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Age:";
+            // 
+            // Agetxtbx
+            // 
+            this.Agetxtbx.Location = new System.Drawing.Point(98, 113);
+            this.Agetxtbx.Name = "Agetxtbx";
+            this.Agetxtbx.Size = new System.Drawing.Size(45, 27);
+            this.Agetxtbx.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Contact No.";
+            // 
+            // Contacttxtbx
+            // 
+            this.Contacttxtbx.Location = new System.Drawing.Point(98, 146);
+            this.Contacttxtbx.Name = "Contacttxtbx";
+            this.Contacttxtbx.Size = new System.Drawing.Size(176, 27);
+            this.Contacttxtbx.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Email:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Emailtxtbx
+            // 
+            this.Emailtxtbx.Location = new System.Drawing.Point(98, 179);
+            this.Emailtxtbx.Name = "Emailtxtbx";
+            this.Emailtxtbx.Size = new System.Drawing.Size(176, 27);
+            this.Emailtxtbx.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Personal Information";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(314, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Address";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(314, 50);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Street Address:";
+            // 
+            // Streettxtbx
+            // 
+            this.Streettxtbx.Location = new System.Drawing.Point(428, 47);
+            this.Streettxtbx.Name = "Streettxtbx";
+            this.Streettxtbx.Size = new System.Drawing.Size(211, 27);
+            this.Streettxtbx.TabIndex = 12;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(314, 83);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(98, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "City / District:";
+            // 
+            // Citytxtbx
+            // 
+            this.Citytxtbx.Location = new System.Drawing.Point(428, 80);
+            this.Citytxtbx.Name = "Citytxtbx";
+            this.Citytxtbx.Size = new System.Drawing.Size(211, 27);
+            this.Citytxtbx.TabIndex = 14;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(314, 116);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(68, 20);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Province:";
+            // 
+            // Provincetxtbx
+            // 
+            this.Provincetxtbx.Location = new System.Drawing.Point(428, 113);
+            this.Provincetxtbx.Name = "Provincetxtbx";
+            this.Provincetxtbx.Size = new System.Drawing.Size(211, 27);
+            this.Provincetxtbx.TabIndex = 16;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(314, 149);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(63, 20);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Country:";
+            // 
+            // Countrytxtbx
+            // 
+            this.Countrytxtbx.Location = new System.Drawing.Point(428, 146);
+            this.Countrytxtbx.Name = "Countrytxtbx";
+            this.Countrytxtbx.Size = new System.Drawing.Size(211, 27);
+            this.Countrytxtbx.TabIndex = 18;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(497, 236);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(142, 56);
+            this.saveBtn.TabIndex = 20;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 304);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.Countrytxtbx);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Provincetxtbx);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.Citytxtbx);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Streettxtbx);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Emailtxtbx);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Contacttxtbx);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Agetxtbx);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LastNametxtbx);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FirstNametxtbx);
+            this.Name = "Form1";
+            this.Text = "Contact Tracing Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FirstNametxtbx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LastNametxtbx;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Agetxtbx;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Contacttxtbx;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Emailtxtbx;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox Streettxtbx;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox Citytxtbx;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox Provincetxtbx;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox Countrytxtbx;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
+

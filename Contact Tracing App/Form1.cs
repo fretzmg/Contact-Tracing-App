6e48eff6364751e50bca28c6a96ff78000f41904
John Fretz Magabilin <magabilinfrets@gmail.com>
2021-05-31 14:00:01 +0800

    display text file in notepad

--- Contact Tracing App/Form1.cs
+++ Contact Tracing App/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -52,8 +53,10 @@ namespace Contact_Tracing_App
 
         private void LoadInfobtn_Click(object sender, EventArgs e)
         {
-            LoadInfo LoadInfo = new LoadInfo();
-            LoadInfo.Show();
+            //LoadInfo LoadInfo = new LoadInfo();
+            //LoadInfo.Show();
+
+            Process.Start("notepad.exe", "Contact Tracing Informations.txt");
         }
     }
 }
